fd87bd463c612bb0014fc0d4c7782a1113d67578
pasogott <pascal@calumba.at>
2026-01-02 09:23:44 +0100

    Update SHA256 for v1.0.0

--- Casks/quicksnip.rb
+++ Casks/quicksnip.rb
@@ -1,6 +1,6 @@
 cask "quicksnip" do
   version "1.0.0"
-  sha256 "16e272a2f207fc0991f5b958c77dce6306766499947c3c44e00db2deef8104e5"
+  sha256 "01574dae7a95b551ad31c32873bfa3752569b6325b994537f30c680783cc5ead"
 
   url "https://github.com/pasogott/quicksnip-menubar-swift/releases/download/v#{version}/QuickSnip.dmg"
   name "QuickSnip"
